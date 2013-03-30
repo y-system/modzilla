c116aa096585b4150013a0b7366e9dbe2f0f9f89
y_system <sky.a68@gmail.com>
2013-03-30 23:10:45 +0900

    first commit

---
+++ data/modmozilla.js
@@ -0,0 +1,2 @@
+with(document.body)innerHTML=innerHTML.replace(/mozzila/g,'mozilla').replace(/mozila/g,'mozilla').replace(/mozzilla/g,'mozilla').replace(/Mozzila/g,'Mozilla').replace(/Mozila/g,'Mozilla').replace(/Mozzilla/g,'Mozilla');
+focus();
